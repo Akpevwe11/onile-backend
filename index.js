26e58ae4ff5a47e5427d7b5786660f71df541d79
topzyray <topzyray@yahoo.com>
2024-01-22 13:22:12 +0100

    Initial commit. Project set up

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express"
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server Listening on port ${port}...`)
+})
