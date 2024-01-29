bd21408c9b230587a53a002d1ffb34296aab1b9d
Akpevwe <essiakpevwepraise@gmail.com>
2024-01-29 20:39:35 +0100

    config server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express"
-import 'dotenv/config'
 
+import dotenv from 'dotenv'
 const app = express()
 const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World'))
 
 app.listen(port, () => {
     console.log(`Server Listening on port ${port}...`)
